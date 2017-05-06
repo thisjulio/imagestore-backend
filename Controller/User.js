b144e2f3a155ab09978220220a0b5d2a6dc8ba28
Julio Cesar <julio-cesar@unicastconsultoria.com.br>
2017-05-06 09:27:56 -0300

    Construção da estrutura inicial de modelos e controllers para usuarios

---
+++ Controller/User.js
@@ -0,0 +1,9 @@
+const UserModel = require("../Model/User");
+
+exports.signup = (name,email,password) => {};
+
+exports.login = (email,password) => {};
+
+exports.recoverPassword = (email) => {};
+
+exports.update = (dataToUpdate) => {};
---
+++ Model/User.js
@@ -0,0 +1,7 @@
+exports.all = () => [];
+
+exports.findById = (id) => null;
+
+exports.findByEmail = (email) => null;
+
+exports.create = (email,name,password) => null;
